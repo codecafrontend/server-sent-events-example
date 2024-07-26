1ff91bef7d2051079894aa53efe60b53a0dbcbc1
olyakrylo <olyakrylo24@gmail.com>
2024-07-26 11:20:40 +0200

    fix connection

--- server/sse.mjs
+++ server/sse.mjs
@@ -14,6 +14,7 @@ export const createChatServer = (app) => {
     let clients = [];
 
     app.get('/sse', (req, res) => {
+        console.log('req')
         res.setHeader('Content-Type', 'text/event-stream');
         res.setHeader('Cache-Control', 'no-cache');
         res.setHeader('Connection', 'keep-alive');
@@ -21,10 +22,15 @@ export const createChatServer = (app) => {
         const id = nanoid();
         clients.push({ id, res });
 
+        console.log(clients.length);
+
         req.on('close', () => {
             clients = clients.filter(client => client.id !== id);
             res.end();
         });
+
+        res.write(`event: connection\n\n`);
+        res.flush();
     });
       
     // Route для отправки сообщений на сервер
